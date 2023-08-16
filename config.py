edd1c6ac9d76025ba92f16810940824e343ea76b
MovieMixology <142192616+MovieMixology@users.noreply.github.com>
2023-08-16 16:37:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention},\n\nYou need to join @MovieMixologyManager Channel to use this bot!\n\n<b>And click on try again to get your file(s)!</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b>Hello {mention}</b>,\n\nYou need to join @MovieMixologyManager Channel to use this bot!\n\n<b>And click on try again to get your file(s)!</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
